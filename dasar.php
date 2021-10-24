725d39bfd22565ab6101d24662f24f8a4878bc18
Nuverin <90313515+nuverin@users.noreply.github.com>
2021-10-24 08:58:19 +0700

    Add files via upload
    membuat halaman dropdown menggunakan php
    #learn

---
+++ dasar.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/052d7cb2b9.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="navbar">
+            <ul>
+                <li><a href="#" class="a_parent">
+                    <div class="wrap">
+                        <span class='icon'>
+                        <i class="fas fa-home"></i>
+                        </span>
+                        <span class='text'>Home</span>
+                    </div>
+                </a></li>
+                <li><a href="#" class="a_parent">
+                    <div class="wrap">
+                        <span class='icon'>
+                            <i class="fas fa-book"></i>
+                        </span>
+                        <span class='text'>Tutorial</span>
+                    </div>
+                </a>
+                 <div class="dd_menu">
+                     <ul>
+                        <li>
+                            <a href="" class='dd_menu_a'>
+                                <div class="wrap">
+                                    <span class='icon'>
+                                        <i class="fas fa-book"></i>
+                                    </span>
+                                    <span class='text'>Python</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class='dd_menu_a'>
+                                <div class="wrap">
+                                    <span class='icon'>
+                                        <i class="fas fa-book"></i>
+                                    </span>
+                                    <span class='text'>HTML</span>
+                                </div>
+                            </a>
+                        </li>  
+                        <li>
+                            <a href="" class='dd_menu_a'>
+                                <div class="wrap">
+                                    <span class='icon'>
+                                        <i class="fas fa-book"></i>
+                                    </span>
+                                    <span class='text'>Materi Kelas 10</span>
+                                </div>
+                            </a>
+                        </li>  
+                        <li>
+                            <a href="" class='dd_menu_a'>
+                                <div class="wrap">
+                                    <span class='icon'>
+                                        <i class="fas fa-book"></i>
+                                    </span>
+                                    <span class='text'>Materi Kelas 11</span>
+                                </div>
+                            </a>
+                        </li>    
+                    </ul>
+                 </div>   
+                </li>
+            <li><a href="#" class="a_parent">
+                <div class="wrap">
+                    <span class='icon'>
+                        <i class="fas fa-box"></i>
+                    </span>
+                    <span class='text'>Drive</span>
+                    </div>
+                </a></li>
+            <li><a href="#" class="a_parent">
+                <div class="wrap">
+                    <span class='icon'>
+                        <i class="fas fa-cog"></i>
+                    </span>
+                    <span class='text'>Settings</span>
+                    </div>
+                </a></li>
+            </ul>
+        </div>
+    </div>
+<script src="script.js"></script>
+</body>
+</html>
